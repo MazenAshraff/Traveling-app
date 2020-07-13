033e75ee388754aa56c85e7a7e3d5467ec306059
Mazen <mazen.ashraf.12@gmail.com>
2020-07-14 00:33:57 +0200

    fetched longtitude and latitude correctly from API

--- src/server/index.js
+++ src/server/index.js
@@ -16,3 +16,14 @@ const listening = () => console.log(`Running on localhost:${port}`);
 app.listen(port, listening);
 const geonamesURL = 'http://api.geonames.org/searchJSON?q=';
 const user = process.env.user;
+app.post('/location', async(req, res) => {
+    const loc = req.body['location'];
+    try {
+        const apiResponse = await fetch(`http://api.geonames.org/searchJSON?q=${loc}&username=${user}`);
+        const jsondata = await apiResponse.json()
+        const long = jsondata.geonames[0].lng;
+        const lat = jsondata.geonames[0].lat;
+    } catch (e) {
+        console.log('This is an  error', e);
+    }
+})
