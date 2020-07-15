6d16d993d56a2305dbd94c6b591466b3c52ccf36
Mazen <mazen.ashraf.12@gmail.com>
2020-07-15 20:49:21 +0200

    modified the server to do many image fetches if needed

--- src/server/index.js
+++ src/server/index.js
@@ -1,3 +1,4 @@
+let projectData = {};
 const express = require('express');
 const bodyparser = require('body-parser');
 const cors = require('cors');
@@ -20,23 +21,57 @@ const geonamesURL = 'http://api.geonames.org/searchJSON?name=';
 const weatherURL = 'https://api.weatherbit.io/v2.0/forecast/daily?'
 const pixaURL = `https://pixabay.com/api/?key=${pixabay_key}&q=`
 
+
+const isEmpty = (elem) => {
+    if (elem.total == 0)
+        return true;
+    else
+        return false;
+
+}
 app.post('/location', async(req, res) => {
     const loc = req.body['location'];
     const day = req.body['date'];
     try {
         const geoResponse = await fetch(`${geonamesURL}${loc}&username=${user}`);
         const geoJsonData = await geoResponse.json();
-        const pixaResponse = await fetch(`${pixaURL}${loc}&image_type=photo&category=travel&order=popular`);
-        const pixaJsonData = await pixaResponse.json();
-        const picture = pixaJsonData.hits[0].webformatURL;
-        const long = geoJsonData.geonames[0].lng;
-        const lat = geoJsonData.geonames[0].lat;
-        const weatherResp = await fetch(`${weatherURL}&lat=${lat}&lon=${long}&key=${weather_key}`);
+        let pixaResponse = await fetch(`${pixaURL}${loc}&image_type=photo&orientation=wider&category=travel&editors_choice=true&order=popular`);
+        let pixaJsonData = await pixaResponse.json();
+        if (isEmpty(pixaJsonData)) {
+            pixaResponse = await fetch(`${pixaURL}${loc}&image_type=photo&orientation=wider&category=travel&order=popular`);
+            pixaJsonData = await pixaResponse.json();
+            if (isEmpty(pixaJsonData)) {
+                pixaResponse = await fetch(`${pixaURL}${loc}&image_type=photo&category=travel&order=popular`);
+                pixaJsonData = await pixaResponse.json();
+                if (isEmpty(pixaJsonData)) {
+                    pixaResponse = await fetch(`${pixaURL}${loc}&image_type=photo&order=popular`);
+                    pixaJsonData = await pixaResponse.json();
+                    if (isEmpty(pixaJsonData)) {
+                        pixaResponse = await fetch(`${pixaURL}${loc}&order=popular`);
+                        pixaJsonData = await pixaResponse.json();
+                        if (isEmpty(pixaJsonData)) {
+                            pixaResponse = await fetch(`${pixaURL}${loc}`);
+                            pixaJsonData = await pixaResponse.json();
+                            if (isEmpty(pixaJsonData)) {
+                                projectData.picture = 'Sorry, we couldn\'t find any images of the location.';
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        if (projectData.picture != 'Sorry, we couldn\'t find any images of the location.') {
+            projectData.picture = pixaJsonData.hits[0].webformatURL;
+
+        }
+        projectData.long = geoJsonData.geonames[0].lng;
+        projectData.lat = geoJsonData.geonames[0].lat;
+        const weatherResp = await fetch(`${weatherURL}&lat=${projectData.lat}&lon=${projectData.long}&key=${weather_key}`);
         const weatherJsonData = await weatherResp.json();
-        const high_temp = weatherJsonData.data[day].high_temp;
-        const low_temp = weatherJsonData.data[day].low_temp;
-        const chanceOfRain = weatherJsonData.data[day].pop;
-        res.send({ high_temp: high_temp, low_temp: low_temp, chanceOfRain: chanceOfRain, picture: picture });
+        projectData.high_temp = weatherJsonData.data[day].high_temp;
+        projectData.low_temp = weatherJsonData.data[day].low_temp;
+        projectData.chanceOfRain = weatherJsonData.data[day].pop;
+        res.send(projectData);
     } catch (e) {
         console.log('This is an  error', e);
     }
