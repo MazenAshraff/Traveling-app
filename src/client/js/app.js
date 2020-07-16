5d07f508cf958bf298e4a30d1d99ba6a626c6e1f
Mazen <mazen.ashraf.12@gmail.com>
2020-07-16 14:42:24 +0200

    added comments and updated the updateUI function

--- src/client/js/app.js
+++ src/client/js/app.js
@@ -14,13 +14,13 @@ const getSomeDate = (offset) => { // a function to get a future or past date bas
     const curDate = modifyCurDate().split('-');
     let someDate = [0, 0, 0];
     if (curDate[0] % 4 == 0)
-        days[2] = 29;
+        days[2] = 29; // This is a leap year, februaries have 29 days instead of 28
     else
         days[2] = 28;
-    if (curDate[2] + offset > days[curDate[2]]) {
+    if (curDate[2] + offset > days[curDate[2]]) { // the offset exceeded the number of days in that month
         someDate[2] = parseInt(curDate[2]) + offset - days[curDate[2]];
         someDate[1] = (parseInt(curDate[1]) + 1 == 13) ? 1 : parseInt(curDate[1]) + 1;
-        if (someDate[1] == 1)
+        if (someDate[1] == 1) // the month became january
             someDate[0] = curDate[0]++;
     } else {
         someDate = curDate;
@@ -30,14 +30,9 @@ const getSomeDate = (offset) => { // a function to get a future or past date bas
     someDate[1] = someDate[1] < 10 ? `0${someDate[1]}` : someDate[1];
     someDate[2] = someDate[2] < 10 ? `0${someDate[2]}` : someDate[2];
     return `${someDate[0]}-${someDate[1]}-${someDate[2]}`;
-    // for (let i = 0; i < 3; i++) {
-    //     if (parseInt(requestedDate[i]) > parseInt(maxDate[i]))
-    //         return false;
-    // }
-    // return true;
 
 }
-const modifyCurDate = () => {
+const modifyCurDate = () => { //set the date to the format accepted by the date tag
     const date = new Date();
     const day = (date.getDate()) < 10 ? `0${date.getDate()}` : date.getDate();
     let month = (date.getMonth() + 1) < 10 ? `0${date.getMonth()+1}` : date.getMonth() + 1;
@@ -45,14 +40,15 @@ const modifyCurDate = () => {
     return `${year}-${month}-${day}`;
 }
 
-const getOffset = () => {
+const getOffset = () => { //map the dates to numbers
     let dates = {};
     for (let i = 0; i < 16; i++) {
         dates[getSomeDate(i)] = i;
     }
     return dates;
 }
-const isValid = (date) => {
+const isValid = (date) => { //check whether the entered departure date will be accepted by the API
+    if (date === '') return false;
     const dateArray = date.split('-');
     const cur_date = modifyCurDate().split('-');
     const max_date = getSomeDate(15).split('-');
@@ -62,18 +58,71 @@ const isValid = (date) => {
     }
     return true;
 }
-
-const test = async() => {
-    const locValue = document.getElementById('location').value;
-    const date = document.getElementById("startdate").value;
-    console.log(date);
-    if (!isValid(date)) {
-        alert('Please Enter a valid date');
-        return null;
+const isGreaterEqual = (returnDate, startDate) => { //check whether the return date is after the start date  
+    if (returnDate === '') return false;
+    const retArr = returnDate.split('-');
+    const startArr = startDate.split('-');
+    for (let i = 0; i < 3; i++) {
+        if (parseInt(retArr[i]) > parseInt(startArr[i]))
+            return true;
+        else if (parseInt(retArr[i]) < parseInt(startArr[i]))
+            return false;
     }
-    const offset = getOffset();
-    postData('http://localhost:8000/location', { location: `${locValue}`, date: offset[date] });
+    return true;
 }
 
+const duration = (startDate, endDate) => { //get the nummber of days between 2 dates
+    const date1 = new Date(startDate);
+    const date2 = new Date(endDate);
+    return Math.floor((date2.getTime() - date1.getTime()) / (1000 * 3600 * 24))
 
-export { postData, test, getSomeDate, modifyCurDate, getOffset }
+}
+
+const updateUI = async() => { //updating the UI using information retrieved from APIs
+    const locValue = document.getElementById('location').value;
+    const departureDate = document.getElementById('startdate').value;
+    const returnDate = document.getElementById('returndate').value;
+    const notes = document.getElementById('notes').value;
+    const startlabel = document.getElementById('startlabel');
+    const endlabel = document.getElementById('endlabel');
+    if (!isValid(departureDate)) {
+        startlabel.innerHTML = 'Departure Date:</br>Please enter a valid date, you can choose any present or future date by maximum 16 days starting today.';
+        startlabel.setAttribute('style', 'color:red;');
+        return null;
+    } else {
+        startlabel.textContent = 'Departure Date:';
+        startlabel.setAttribute('style', '');
+    }
+    if (!isGreaterEqual(returnDate, departureDate)) {
+        endlabel.innerHTML = 'Return Date:</br>Please enter a valid return date!';
+        endlabel.setAttribute('style', 'color:red;');
+        return null;
+
+    } else {
+        endlabel.innerHTML = 'Return Date:';
+        endlabel.setAttribute('style', '');
+    }
+    const offset = getOffset();
+    console.log(offset[departureDate]);
+    const serverData = (await (await postData('http://localhost:8000/location', { location: `${locValue}`, date: offset[departureDate] })).json());
+    document.getElementById('recievedlocation').innerHTML = `<i class="fas fa-map-marked-alt" style="font-size:1.5em;color:#0CCA4A;"></i>Location: ${locValue}`
+    document.getElementById('hightemp').innerHTML = `<i class="fas fa-temperature-high" style="font-size:1.5em;color:#D90368";></i>High Tempreature on Arrival Date: ${serverData.high_temp}°C`;
+    document.getElementById('lowtemp').innerHTML = `<i class="fas fa-temperature-low" style="font-size:1.5em;color:#5FBFF9;"></i>Low Temperature on Arrival Date: ${serverData.low_temp}°C`;
+    document.getElementById('chanceofrain').innerHTML = `<i class='fas fa-cloud-rain' style="font-size:1.5em;color:#00487C"></i>    Chance of Rain on Arrival Date: ${serverData.chanceOfRain}%`;
+    document.getElementById('container').setAttribute('style', '');
+    document.getElementById('departure').innerHTML = `<i class='far fa-calendar' style="font-size:1.5em;color:#0"></i>Departure Date:${departureDate}`;
+    document.getElementById('return').innerHTML = `<i class='far fa-calendar' style='font-size:1.5em;color:#0'></i>Return Date:${returnDate}`;
+    document.getElementById('duration').innerHTML = `<i class='fas fa-clock' style='font-size:1.5em;color:yellow'></i>Duration:${duration(departureDate,returnDate)} Days`
+    document.getElementById('thetext').value = notes;
+    document.getElementById('popularimage').setAttribute('src', `${ serverData.picture }`);
+    console.log('done');
+}
+
+const returndatelimiter = (() => {
+        document.getElementById('startdate').addEventListener('input', (e) => {
+            document.getElementById('returndate').setAttribute('min', document.getElementById('startdate').value);
+        });
+    }
+
+)();
+export { postData, updateUI, getSomeDate, modifyCurDate, getOffset, returndatelimiter, isGreaterEqual }
